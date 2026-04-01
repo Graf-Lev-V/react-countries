2262b05019e150d16aa062bb254a9f8af4ec34dd
Graf-Lev-V <legendarlev2004@gmail.com>
2026-04-02 01:30:22 +0300

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/react-countries/'>
+    <HashRouter basename='/react-countries'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
