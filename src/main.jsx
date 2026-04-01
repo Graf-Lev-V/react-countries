8bf0987944b6cdd444369e177a6dddad5241b7b3
Graf-Lev-V <legendarlev2004@gmail.com>
2026-04-02 01:35:05 +0300

    123

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename='/react-countries'>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
